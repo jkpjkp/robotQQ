dba1c0ea386edbc092c49374b655a613040d3ea0
迪越-焦鲲鹏 <jiaoqp@qq.com>
2022-02-28 12:47:33 +0800

    网易云-1

--- getMusic.py
+++ getMusic.py
@@ -3,20 +3,28 @@ import json
 import traceback
 
 import requests
+# UserAgent生成库
+from fake_useragent import UserAgent
 
 
 def getMusic(keywords):
     # 设置url
     url = 'http://120.25.223.76:3000/search?'
+    headers = {
+        'User-Agent': UserAgent().random
+    }
     params = {
         'keywords': keywords
     }
     try:
         # 执行请求
-        response = requests.get(url=url, params=params)
+        response = requests.get(url=url, headers=headers, params=params)
         text = response.text
         eva = json.loads(text)
         musicId = eva['result']['songs'][0]['id']
         return musicId
     except Exception as e:
         traceback.print_exc()
+
+if __name__ == '__main__':
+    print(getMusic('onelastkiss'))
